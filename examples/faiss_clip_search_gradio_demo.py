4e50f5ed951b66303cdda592fa21ff9dd0a54865
shibing624 <shibing624@126.com>
2023-09-21 22:39:44 +0800

    update gradio demo.

--- examples/faiss_clip_search_gradio_demo.py
+++ examples/faiss_clip_search_gradio_demo.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 @author:XuMing(xuming624@qq.com)
-@description: 
+@description:
 """
 
 import base64
@@ -24,15 +24,23 @@ def image_path_to_base64(image_path: str) -> str:
 
 
 def search_image(text="", image=None):
+    html_output = ""
+
+    if not text and not image:
+        return "<p>Please provide either text or image input.</p>"
+
     if text and image is not None:
         return "<p>Please provide either text or image input, not both.</p>"
 
     if image is not None:
         results = client.search(image=image)
+        image_src = "data:image/jpeg;base64," + image_path_to_base64(image)
+        html_output += f'Query: <img src="{image_src}" width="200" height="200"><br>'
     else:
         results = client.search(text=text)
+        html_output += f'Query: {text}<br>'
 
-    html_output = ""
+    html_output += f'Result Size: {len(results)}<br>'
     for result in results:
         item, similarity_score, _ = result
         item_dict = json.loads(item)
@@ -47,25 +55,36 @@ def search_image(text="", image=None):
         html_output += f'<div style="display: inline-block; position: relative; margin: 10px;">'
         html_output += f'<img src="{image_src}" width="200" height="200" title="{tip}">'
         html_output += f'<div style="position: absolute; bottom: 0; right: 0; background-color: rgba(255, 255, 255, 0.7); padding: 2px 5px;">'
-        html_output += f'Score: {similarity_score:.2f}'
+        html_output += f'Score: {similarity_score:.4f}'
         html_output += f'</div></div>'
 
     return html_output
 
 
 def main():
-    iface = gr.Interface(
-        fn=search_image,
-        inputs=[
-            gr.inputs.Textbox(lines=3, placeholder="Enter text here..."),
-            gr.inputs.Image(type="filepath", label="Upload an image"),
-        ],
-        outputs=gr.outputs.HTML(label="Search results"),
-        title="CLIP Image Search",
-        description="Search for similar images using Faiss and Chinese-CLIP. Link to Github: [similarities](https://github.com/shibing624/similarities)",
-    )
+    def reset_user_input():
+        return '', None
 
-    iface.launch(server_name='0.0.0.0', server_port=8082)
+    with gr.Blocks() as demo:
+        gr.HTML("""<h1 align="center">CLIP Image Search</h1>""")
+        gr.Markdown(
+            "> Search for similar images using Faiss and Chinese-CLIP. Link to Github: [similarities](https://github.com/shibing624/similarities)")
+        with gr.Tab("Text"):
+            with gr.Row():
+                with gr.Column():
+                    input_text = gr.Textbox(lines=2, placeholder="Enter text here...")
+
+        with gr.Tab("Image"):
+            with gr.Row():
+                with gr.Column():
+                    input_image = gr.Image(type="filepath", label="Upload an image")
+
+        btn_submit = gr.Button(label="Submit")
+        output = gr.outputs.HTML(label="Search results")
+        btn_submit.click(search_image, inputs=[input_text, input_image], outputs=output, show_progress=True)
+        btn_submit.click(reset_user_input, outputs=[input_text, input_image])
+
+    demo.queue().launch(server_name='0.0.0.0', server_port=8082, share=False)
 
 
 if __name__ == '__main__':
