2840e0d6b99d1cb45857809df78b704b527956d0
shibing624 <shibing624@126.com>
2023-11-14 18:30:14 +0800

    update image and text duplicates demo.

--- examples/text_similarity_demo.py
+++ examples/text_similarity_demo.py
@@ -9,5 +9,21 @@ sys.path.append('..')
 from similarities import BertSimilarity
 
 m = BertSimilarity()
-r = m.similarity('如何更换花呗绑定银行卡', '花呗更改绑定银行卡')
-print(f"similarity score: {float(r)}")
+sentences1 = [
+    '如何更换花呗绑定银行卡',
+    '花呗更改绑定银行卡',
+]
+sentences2 = [
+    '花呗更改绑定银行卡',
+    '我什么时候开通了花呗',
+]
+
+similarity_scores = m.similarity(sentences1, sentences2)
+print(f"similarity score: {similarity_scores.numpy()}")
+if len(sentences1) == len(sentences2):
+    for i in range(len(sentences1)):
+        print(f"{sentences1[i]} vs {sentences2[i]}, score: {similarity_scores.numpy()[i][i]:.4f}")
+else:
+    for i in range(len(sentences1)):
+        for j in range(len(sentences2)):
+            print(f"{sentences1[i]} vs {sentences2[j]}, score: {similarity_scores.numpy()[i][j]:.4f}")
