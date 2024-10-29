594ee0e76a1427b22d05fe17ad36587dbafceed4
shibing624 <shibing624@126.com>
2024-10-29 22:09:11 +0800

    update demo.

--- examples/search_gradio_demo.py
+++ examples/search_gradio_demo.py
@@ -9,11 +9,14 @@ from similarities import BertSimilarity
 
 sim_model = BertSimilarity()
 
+
 def load_file(path):
     with open(path, 'r', encoding='utf-8') as f:
         return f.read().split('\n')
 
+
 sim_model.add_corpus(load_file('data/corpus_100.txt'))
+
 
 def ai_text(query):
     res = sim_model.most_similar(queries=query, topn=5)
@@ -22,8 +25,10 @@ def ai_text(query):
         print('query:', query)
         print("search top 5:")
         print(f'\t{c}')
-    res_show = '\n'.join(['search top5：'] + [f'text: {k.get("corpus_doc")} score: {k.get("score"):.4f}' for k in res[0]])
+    res_show = '\n'.join(
+        ['search top5：'] + [f'text: {k.get("corpus_doc")} score: {k.get("score"):.4f}' for k in res[0]])
     return res_show
+
 
 if __name__ == '__main__':
     examples = [
@@ -32,12 +37,10 @@ if __name__ == '__main__':
         ['她在看书'],
         ['一个人弹琴'],
     ]
-    input = gr.Textbox(lines=2, placeholder="Enter Query")
-    output_text = gr.Textbox()
     gr.Interface(
         ai_text,
-        inputs=input,
-        outputs=output_text,
+        inputs=gr.Textbox(lines=2, label="Enter Query"),
+        outputs=gr.Textbox(label="Output Box"),
         title="Chinese Text Semantic Search Model",
         description="Copy or input Chinese text here. Submit and the machine will find the most similarity texts.",
         article="Link to <a href='https://github.com/shibing624/similarities'  style='color:blue;' target='_blank'>Github REPO</a>",
